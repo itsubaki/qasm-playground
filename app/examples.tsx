fb5b8ee14a0c671b798fad914f35fc8cb2c99826
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-08-17 13:33:46 +0000

    Add Deutsch-Jozsa Algorithm

--- app/examples.tsx
+++ app/examples.tsx
@@ -87,6 +87,39 @@ swap(q);
 `,
   },
   {
+    name: "Deutsch-Jozsa Algorithm",
+    code: `// Deutsch-Jozsa Algorithm
+
+OPENQASM 3.0;
+
+gate x q { U(pi, 0, pi) q; }
+gate h q { U(pi/2.0, 0, pi) q; }
+gate cx q0, q1 { ctrl @ U(pi, 0, pi) q0, q1; }
+
+def constant(qubit q0, qubit q1) {
+    x q1;
+}
+
+def balanced(qubit q0, qubit q1) {
+    cx q0, q1;
+}
+
+qubit q0;
+qubit q1;
+reset q0;
+reset q1;
+
+x q1;
+h q0;
+h q1;
+
+balanced(q0, q1);
+
+h q0;
+measure q0;
+`
+  },
+  {
     name: "Grover's Algorithm",
     code: `// Grover's Algorithm
 
@@ -131,8 +164,8 @@ measure q[3];
 `,
   },
   {
-    name: "Sudoku 2x2",
-    code: `// Sudoku 2x2
+    name: "Grover's Algorithm (Sudoku 2x2)",
+    code: `// Grover's Algorithm (Sudoku 2x2)
 
 OPENQASM 3.0;
 
