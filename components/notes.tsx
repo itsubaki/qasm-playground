a1c05c15b9d244834c8093cd4456cae2f77054a4
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-11-23 22:41:43 +0900

    Update CSS in notes

--- components/notes.tsx
+++ components/notes.tsx
@@ -3,9 +3,11 @@ export function Notes({
 }: {
     isDarkMode: boolean,
 }) {
+    const transition = "transition-colors duration-500"
+
     return (
         <div className="text-left">
-            <ul className={`text-sm space-y-1 transition-colors duration-500 ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
+            <ul className={`text-sm space-y-1 ${transition} ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
                 <li>Any unitary operation requires exponential resources with respect to the number of qubits.</li>
                 <li>This playground supports up to 10 qubits. If you want to lift this limitation, please self-host it.</li>
                 <li>There are several other limitations. For more details, please refer to{" "}
@@ -13,7 +15,7 @@ export function Notes({
                         href="https://github.com/itsubaki/qasm/issues"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className={`transition-colors duration-500 ${isDarkMode ? "text-blue-400 underline" : "text-blue-600 underline"}`}
+                        className={`${transition} ${isDarkMode ? "text-blue-400 underline" : "text-blue-600 underline"}`}
                     >
                         the issues
                     </a>
