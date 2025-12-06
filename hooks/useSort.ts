dad7bac1d33c3d63d8911b9d34f99daf26a3767f
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-12-06 13:55:22 +0900

    Update some files

--- hooks/useSort.ts
+++ hooks/useSort.ts
@@ -3,12 +3,12 @@ import { useState } from "react"
 const modes = ["index", "prob_desc"] as const
 
 export const useSort = () => {
-    const [sortMode, setMode] = useState<typeof modes[number]>("index")
+    const [sortMode, setSortMode] = useState<typeof modes[number]>("index")
 
     const sort = () => {
         const currentIndex = modes.indexOf(sortMode)
         const next = modes[(currentIndex + 1) % modes.length]
-        setMode(next)
+        setSortMode(next)
     }
 
     return {
