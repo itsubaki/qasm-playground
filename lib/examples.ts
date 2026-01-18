070c52221cdf90afc4d41754648e2d2eb4100983
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-01-18 22:31:47 +0900

    Update example

--- lib/examples.ts
+++ lib/examples.ts
@@ -147,9 +147,10 @@ gate x q { U(pi, 0, pi) q; }
 gate h q { U(pi/2.0, 0, pi) q; }
 
 gate cx q0, q1 { ctrl @ U(pi, 0, pi) q0, q1; }
+gate xor q0, q1, q2 { cx q0, q2; cx q1, q2; }
+
 gate cccz c0, c1, c2, t { ctrl(3) @ U(0, pi, 0) c0, c1, c2, t; }
 gate ccccz c0, c1, c2, c3, t { ctrl(4) @ U(0, pi, 0) c0, c1, c2, c3, t; }
-gate xor q0, q1, q2 { cx q0, q2; cx q1, q2; }
 
 // The oracle constructs a Grover oracle that checks solutions to a 2x2 sudoku puzzle.
 // The oracle flips the phase when the following uniqueness constraints are satisfied: a != b, c != d, a != c, and b != d.
@@ -162,10 +163,10 @@ def oracle(qubit[4] r, qubit[4] s, qubit a) {
 
     ccccz s[0], s[1], s[2], s[3], a;
 
-    xor r[3], r[1], s[3];
-    xor r[2], r[0], s[2];
-    xor r[3], r[2], s[1];
-    xor r[1], r[0], s[0];
+    xor r[1], r[3], s[3];
+    xor r[0], r[2], s[2];
+    xor r[2], r[3], s[1];
+    xor r[0], r[1], s[0];
 }
 
 def diffuser(qubit[4] r) {
