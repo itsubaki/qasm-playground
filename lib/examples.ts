1614624cbe0e3d02e82a0807e743074032ab0e49
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-01-24 15:52:39 +0900

    Update examples.ts

--- lib/examples.ts
+++ lib/examples.ts
@@ -97,25 +97,27 @@ OPENQASM 3.0;
 
 gate x q { U(pi, 0, pi) q; }
 gate h q { U(pi/2.0, 0, pi) q; }
-gate cccx c0, c1, c2, t { ctrl(3) @ U(pi, 0, pi) c0, c1, c2, t; }
+gate cccz c0, c1, c2, t { ctrl(3) @ U(0, 0, pi) c0, c1, c2, t; }
 
 // oracle for |110>|x>
 def oracle(qubit[3] q, qubit a) {
-    x q[2]; x a;
-    h a;
-    cccx q[0], q[1], q[2], a;
-    h a;
-    x q[2]; x a;
+    x q[2];
+    x a;
+    cccz q[0], q[1], q[2], a;
+    x a;
+    x q[2];
 }
 
 def diffuser(qubit[3] q, qubit a) {
-    h q; h a;
-    x q; x a;
+    h q;
     h a;
-    cccx q[0], q[1], q[2], a;
+    x q;
+    x a;
+    cccz q[0], q[1], q[2], a;
+    x a;
+    x q;
     h a;
-    x q; x a;
-    h q; h a;
+    h q;
 }
 
 const int n = 3;
@@ -134,8 +136,6 @@ for int i in [1:R] {
     oracle(q, a);
     diffuser(q, a);
 }
-
-measure a;
 `,
     },
     {
