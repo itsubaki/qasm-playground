1f62304e3e67f8072f6ce288fdabfcca63003fb9
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-03-15 19:21:55 +0900

    Update notes

--- components/notes.tsx
+++ components/notes.tsx
@@ -3,7 +3,7 @@ export function Notes() {
         <div className="text-left">
             <ul className={`text-sm space-y-1 text-gray-600 dark:text-gray-400`}>
                 <li>Simulation of arbitrary unitary operations requires resources that grow exponentially with the number of qubits.</li>
-                <li>This playground supports up to 10 qubits, and to remove this limit, please self-host the system.</li>
+                <li>This playground supports up to 12 qubits, and to remove this limit, please self-host the system.</li>
                 <li>Additional limitations apply. For details, see
                     {" "}
                     <a
