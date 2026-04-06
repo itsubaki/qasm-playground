802b04cac0780dea8119a67e77966c4400f8c860
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-06 21:26:31 +0900

    Update notes

--- components/notes.tsx
+++ components/notes.tsx
@@ -2,9 +2,8 @@ export function Notes() {
     return (
         <div className="text-left">
             <ul className={`text-sm space-y-1 text-gray-600 dark:text-gray-400`}>
-                <li>Simulation of arbitrary unitary operations requires resources that grow exponentially with the number of qubits.</li>
-                <li>This playground supports up to 12 qubits, and to remove this limit, please self-host the system.</li>
-                <li>Additional limitations apply. For details, see
+                <li>A playground that lets you write and run OpenQASM code in a web browser.</li>
+                <li>It supports up to 12 qubits, with additional limitations. For more details, see the GitHub
                     {" "}
                     <a
                         href="https://github.com/itsubaki/qasm/issues"
@@ -12,10 +11,9 @@ export function Notes() {
                         rel="noopener noreferrer"
                         className={`underline text-blue-600 dark:text-blue-400`}
                     >
-                        the issues
+                        issues
                     </a>
-                    {" "}
-                    on GitHub.
+                    .
                 </li>
             </ul>
         </div>
