d5403377409eb9c44ad31fcdfb8f3dbfd7453f33
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-20 14:38:20 +0900

    Update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,23 @@ const inter = Inter({
 export const metadata: Metadata = {
     title: "OpenQASM Playground",
     description: "A playground for running OpenQASM code in the browser",
+    icons: {
+        icon: [
+            {
+                url: '/icon-light-32x32.png',
+                media: '(prefers-color-scheme: light)',
+            },
+            {
+                url: '/icon-dark-32x32.png',
+                media: '(prefers-color-scheme: dark)',
+            },
+            {
+                url: '/icon.svg',
+                type: 'image/svg+xml',
+            },
+        ],
+        apple: '/apple-icon.png',
+    },
 }
 
 export default function RootLayout({
